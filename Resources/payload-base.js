82e3c078dda28f4a9debe97e11ec3c43c36aedd0
pylixonly <82711525+pylixonly@users.noreply.github.com>
2024-11-09 13:44:30 +0800

    chore: bump ver to 0.3.2
    for testing

--- Resources/payload-base.js
+++ Resources/payload-base.js
@@ -1,6 +1,6 @@
 globalThis.__PYON_LOADER__ = {
     loaderName: "BunnyTweak",
-    loaderVersion: "0.3.1",
+    loaderVersion: "0.3.2",
     hasThemeSupport: true,
     storedTheme: null,
     fontPatch: 2
